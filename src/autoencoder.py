739774b3654d734092c0c02b4564d92a207aafe9
flavioemanuel859 <flavioemanuel859@gmail.com>
2022-05-22 15:13:10 -0300

    autoencoder.py

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -3,11 +3,10 @@ from keras import layers
 
 encoding_dim = 32
 input_img = keras.Input(shape=(784,))
-print("sadasd")
 
-encoded = layers.Dense(encoding_dim, activation='relu')(input_img)
-# "decoded" is the lossy reconstruction of the input
-decoded = layers.Dense(784, activation='sigmoid')(encoded)
+encoded = layers.Dense(encoding_dim, activation='linear')(input_img)
+decoded = layers.Dense(784, activation='linear')(encoded)
 
-# This model maps an input to its reconstruction
 autoencoder = keras.Model(input_img, decoded)
+
+print("test")
