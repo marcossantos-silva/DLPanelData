acdb97954955f72399008ec0529828045a883cff
flavioemanuel859@gmail.com <flavioemanuel859@gmail.com>
2022-05-24 07:55:01 -0300

    reniao 24/05

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -1,5 +1,20 @@
 import keras
 from keras import layers
+import pandas as pd
+import seaborn as sb
+
+# Min-max Normalization
+'''
+df_min_max_scaled = df.copy()
+  
+
+for column in df_min_max_scaled.columns:
+    df_min_max_scaled[column] = (df_min_max_scaled[column] - df_min_max_scaled[column].min()) / (df_min_max_scaled[column].max() - df_min_max_scaled[column].min())    
+  
+print(df_min_max_scaled)
+'''
+
+#df = pd.read_csv()
 
 encoding_dim = 32
 input_img = keras.Input(shape=(784,))
@@ -8,5 +23,28 @@ encoded = layers.Dense(encoding_dim, activation='linear')(input_img)
 decoded = layers.Dense(784, activation='linear')(encoded)
 
 autoencoder = keras.Model(input_img, decoded)
+encoder = keras.Model(input_img, encoded)
+encoder = keras.Model(input_img, encoded)
 
-print("test")
+encoded_input = keras.Input(shape=(encoding_dim,))
+decoder_layer = autoencoder.layers[-1]
+decoder = keras.Model(encoded_input, decoder_layer(encoded_input))
+autoencoder.compile(optimizer='rmsprop', loss='mse')
+
+from keras.datasets import mnist
+import numpy as np
+(x_train, _), (x_test, _) = mnist.load_data()
+
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+print(x_train.shape)
+print(x_test.shape)
+
+autoencoder.fit(x_train, x_train,
+                epochs=50,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
